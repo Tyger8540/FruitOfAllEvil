29745ff92799e21ebec5944ec39aa508c40eeedb
Tyger8540 <tymatson2015@gmail.com>
2025-02-08 01:59:16 -0800

    Created Circle One Scene w/ Charon

--- scripts/state.gd
+++ scripts/state.gd
@@ -2,10 +2,10 @@ extends Node
 
 
 var location: String = "intro"
-var section: String = "C0_intro"
+var section: String = "C1_intro"
 var vendor: String = ""
 var dialogue_ready: bool = true
-var dialogue_file: String = "res://dialogue/prologue.dialogue"
+var dialogue_file: String = "res://dialogue/circle1.dialogue"
 var level_defeated: bool = false
 
 var circle_num: int = 0
