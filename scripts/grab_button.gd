e3083276a38cdb120aca7393a35e1d03c7bb478e
Tyger8540 <tymatson2015@gmail.com>
2025-03-07 17:06:55 -0800

    Highlight Fruit Boxes

--- scripts/grab_button.gd
+++ scripts/grab_button.gd
@@ -3,18 +3,41 @@ extends TextureButton
 
 
 const GRABBABLE_SCENE = preload("res://scenes/grabbable.tscn")
+const FADE_FACTOR = 0.5
+const LOW_FADE_BOUND = 1.0
+const HIGH_FADE_BOUND = 1.25
+const SCALE_FACTOR = 1.25
 
 @export var fruit: Enums.Fruit_Type
 @export var grab_type: Enums.Grabbable_Type
 
+var hovering: bool = false
+var fading_in: bool = false
+var fading_out: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	pivot_offset = Vector2(size.x / 2, size.y)  # Scales up from the center of the object
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	# Logic for highlighting on hover
+	if hovering:
+		if fading_out:
+			modulate.v -= FADE_FACTOR * delta
+			#modulate.a -= FADE_FACTOR * delta
+			if modulate.v <= LOW_FADE_BOUND:
+				modulate.v = LOW_FADE_BOUND
+				fading_in = true
+				fading_out = false
+		elif fading_in:
+			modulate.v += FADE_FACTOR * delta
+			if modulate.v >= HIGH_FADE_BOUND:
+				modulate.v = HIGH_FADE_BOUND
+				fading_in = false
+				fading_out = true
 
 
 func _on_button_up() -> void:
@@ -41,3 +64,18 @@ func grab_fruit() -> void:
 	grabbable.initialize(fruit_array, grab_type)
 	get_tree().get_root().add_child(grabbable)
 	Globals.is_grabbing = true
+
+
+func _on_mouse_entered() -> void:
+	hovering = true
+	fading_in = true
+	fading_out = false
+	scale *= SCALE_FACTOR
+
+
+func _on_mouse_exited() -> void:
+	hovering = false
+	fading_in = false
+	fading_out = false
+	modulate.v = LOW_FADE_BOUND
+	scale /= SCALE_FACTOR
