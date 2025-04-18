804c8690b4bbcfc8f4aae090142dbf88b626a8b0
Tyger8540 <tymatson2015@gmail.com>
2025-04-17 23:45:37 -0700

    Added New Chopped Blueberries & Grapes

--- scripts/state.gd
+++ scripts/state.gd
@@ -4,13 +4,13 @@ extends Node
 var location: String = "intro"
 var section: String = "C2_intro"
 var vendor: String = ""
-var dialogue_ready: bool = false  # Should be set to true by default
+var dialogue_ready: bool = true  # Should be set to true by default
 var dialogue_file: String = "res://dialogue/circle2.dialogue"
 var level_defeated: bool = false
 var cutscene_speaker: String = ""
 
 var circle_num: int = 2
-var next_boss: String = "cerberus"
+var next_boss: String = "Cerberus"  # MAKE SURE FIRST LETTER IS CAPITALIZED
 var current_boss: String = ""
 var boss_queued: bool = false
 
