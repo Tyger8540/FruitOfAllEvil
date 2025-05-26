8f38040a6105e6ae607b16832808ff496ba0e09a
Tyger8540 <tymatson2015@gmail.com>
2025-05-26 02:44:17 -0700

    Connected Scenes & Updated Earlier UI

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -11,12 +11,34 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_restart_button_button_up() -> void:
+func _on_start_button_button_up() -> void:
 	if State.location == "intro":
 		get_tree().change_scene_to_file("res://scenes/fullscreen_cutscene.tscn")
-	else:
-		get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+
+func _on_restart_button_button_up() -> void:
+	match State.circle_num:
+		0:
+			State.location = "level"
+			State.section = "C0_level_virgil"
+			State.cutscene_speaker = "Virgil"
+			get_tree().change_scene_to_file("res://scenes/prologue.tscn")
+		1:
+			State.location = "level"
+			State.section = "C1_level_virgil1"
+			State.cutscene_speaker = "Virgil"
+			get_tree().change_scene_to_file("res://scenes/circle_one.tscn")
+		2:
+			State.location = "level"
+			State.section = "C2_level_virgil1"
+			State.cutscene_speaker = "DJ Virgil"
+			State.dialogue_ready = true
+			get_tree().change_scene_to_file("res://scenes/circle_two.tscn")
 
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_settings_button_button_up() -> void:
+	pass # Replace with function body.
--- scripts/state.gd
+++ scripts/state.gd
@@ -1,16 +1,17 @@
 extends Node
 
 
+#region Circle 0 - Prologue
 var location: String = "intro"
-var section: String = "C2_intro1"
+var section: String = "C0_intro"
 var vendor: String = ""
 var dialogue_ready: bool = true  # Should be set to true by default
-var dialogue_file: String = "res://dialogue/circle2.dialogue"
+var dialogue_file: String = "res://dialogue/prologue.dialogue"
 var level_defeated: bool = false
 var cutscene_speaker: String = ""
 
-var circle_num: int = 2
-var next_boss: String = "Cerberus"  # MAKE SURE FIRST LETTER IS CAPITALIZED
+var circle_num: int = 0
+var next_boss: String = "Charon"  # MAKE SURE FIRST LETTER IS CAPITALIZED
 var current_boss: String = ""
 var boss_queued: bool = false
 
@@ -19,3 +20,46 @@ var previous_vendor: String = ""
 
 var received_blender: bool = false
 var talked_to_virgil: bool = false
+#endregion
+
+#region Circle 1 - Limbo
+#var location: String = "intro"
+#var section: String = "C1_intro"
+#var vendor: String = ""
+#var dialogue_ready: bool = true  # Should be set to true by default
+#var dialogue_file: String = "res://dialogue/circle1.dialogue"
+#var level_defeated: bool = false
+#var cutscene_speaker: String = ""
+#
+#var circle_num: int = 1
+#var next_boss: String = "Charon"  # MAKE SURE FIRST LETTER IS CAPITALIZED
+#var current_boss: String = ""
+#var boss_queued: bool = false
+#
+#var next_vendor: String = ""
+#var previous_vendor: String = ""
+#
+#var received_blender: bool = false
+#var talked_to_virgil: bool = false
+#endregion
+
+#region Circle 2 - Lust
+#var location: String = "intro"
+#var section: String = "C2_intro1"
+#var vendor: String = ""
+#var dialogue_ready: bool = true  # Should be set to true by default
+#var dialogue_file: String = "res://dialogue/circle2.dialogue"
+#var level_defeated: bool = false
+#var cutscene_speaker: String = ""
+#
+#var circle_num: int = 2
+#var next_boss: String = "Cerberus"  # MAKE SURE FIRST LETTER IS CAPITALIZED
+#var current_boss: String = ""
+#var boss_queued: bool = false
+#
+#var next_vendor: String = ""
+#var previous_vendor: String = ""
+#
+#var received_blender: bool = false
+#var talked_to_virgil: bool = false
+#endregion
